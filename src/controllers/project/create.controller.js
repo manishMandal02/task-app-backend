a5acc3f1ddcb4e65b259cf000c227c9332afc6b8
Manish Mandal <manish.mandal@layrins.com>
2022-06-18 20:22:55 +0530

    minor fix

--- src/controllers/project/create.controller.js
+++ src/controllers/project/create.controller.js
@@ -1,3 +1,17 @@
 const expressAsyncHandler = require('express-async-handler');
+const { Project } = require('../../models/project.model');
 
-module.exports = expressAsyncHandler((req,res))
+module.exports = expressAsyncHandler(async (req, res) => {
+  const { title, desc } = req.body;
+  const userId = req.user._id;
+
+  const project = Project.create({
+    title,
+    desc,
+    owner: userId,
+  });
+  if(project) {
+    res.status(201)
+    res.json({message: ''})
+  }
+});
--- src/utils/generateToken.js
+++ src/utils/generateToken.js
@@ -1,6 +1,6 @@
 const jwt = require('jsonwebtoken');
 
-module.exports.generateToken = (id, time = '30d') => {
+module.exports = (id, time = '30d') => {
   return jwt.sign({ id }, process.env.JWT_SECRET, {
     expiresIn: time,
   });
